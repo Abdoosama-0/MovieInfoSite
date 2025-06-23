085c8757b9b58a1898a2af57292c3fc6522751ca
Abdelrhman Osama Mostafa Shawky <abdalrhman.osama.mostafa@gmail.com>
2025-06-23 18:05:58 +0100

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,7 +7,7 @@ const Nav  = () => {
   return (
   <nav className="sticky    z-[9999]  bg-slate-900 flex flex-row justify-around w-full  items-center p-2  flex-wrap">
 
-    <Link href={`/`}><h1 className='font-[Fantasy]    text-white text-4xl ' >MovieLovers1</h1></Link>
+    <Link href={`/`}><h1 className='font-[Fantasy]    text-white text-4xl ' >MovieLovers</h1></Link>
   
 
     <Searchy/>
