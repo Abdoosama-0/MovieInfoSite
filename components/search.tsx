d98b8eb18a4ecee349a4cd13fe4cf6d4520a4be3
Abdelrhman Osama <abdoosamaplayer11@gmail.com>
2025-07-08 21:40:35 +0100

    1

--- components/search.tsx
+++ components/search.tsx
@@ -29,6 +29,7 @@ const Searchy = () => {
 
   const [data, setData] = useState<Movie[]>([]);
   const fetchMovies = async () => {
+    if(data.length > 0) return; 
     const requests = Array.from({ length: numberOfPages }, (_, i) =>
       fetch(
         `https://api.themoviedb.org/3/movie/top_rated?api_key=caa8300bc818e7643ea53ed6f19509f7&language=en-US&page=${i + 1
